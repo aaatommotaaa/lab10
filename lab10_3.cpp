a5dbd56e15f97dbced52076f95d50d1454eab609
Atom <chanidapa.atom@gmail.com>
2025-12-23 16:52:57 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int count=0;
+    double mean,devia,sum=0,sumpw=0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline)){
+        double x = atof(textline.c_str());
+        sum = sum + x;
+        sumpw = sumpw + pow(x,2);
+        count++;
+    }
+    
+    mean = sum/count;
+    devia = sqrt((sumpw/count)-pow(mean,2));
+
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << mean << '\n';
+    cout << "Standard deviation = " << devia;
+}
